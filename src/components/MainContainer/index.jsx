48f527d290caf9b184d84a8a7ef76ffae3a3cbe0
svandu <svandu08@gmail.com>
2022-12-17 20:41:02 +0530

    maincontainer created

---
+++ src/components/MainContainer/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+//styles
+import './maincontainer.css';
+
+function MainContainer() {
+  return (
+    <div className='main-container'>
+      fjdkfh
+    </div>
+  )
+}
+
+export default MainContainer
