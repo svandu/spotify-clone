b643924630db35e48e512b5c641a5e6b78e702ac
svandu <svandu08@gmail.com>
2022-12-17 20:40:06 +0530

    authentication footer created

---
+++ src/components/AuthFooter/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function AuthFooter() {
+  return (
+    <div className="container">
+      <div className="footer-content">
+        <p>preview of spotify</p>
+        <p>
+          sign up to get unlimited songs and podcasts with occasional adds. no
+          credits card needed.
+        </p>
+      </div>
+      <button className="footer-btn">
+        <span>sign up free</span>
+      </button>
+    </div>
+  );
+}
+
+export default AuthFooter;
