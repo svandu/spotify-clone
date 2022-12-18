a12f14a61eab8d2a2d7d63856b33357ef0018ec5
svandu <svandu08@gmail.com>
2022-12-18 22:47:15 +0530

    main container layout created

--- src/components/MainContainer/index.jsx
+++ src/components/MainContainer/index.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React from "react";
+import ContainerHeroSection from "../ContainerHeroSection";
+import MainNav from "../ui/MainNav";
 
 //styles
-import './maincontainer.css';
+import "./maincontainer.css";
 
 function MainContainer() {
   return (
-    <div className='main-container'>
-      fjdkfh
+    <div className="main-container">
+        <MainNav />
+        <ContainerHeroSection />
     </div>
-  )
+  );
 }
 
-export default MainContainer
+export default MainContainer;
