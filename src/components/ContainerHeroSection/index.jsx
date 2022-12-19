8f4de28646c89b12bf59c4234ef11bf31fa2c0aa
svandu <svandu08@gmail.com>
2022-12-19 23:28:37 +0530

    cards created

--- src/components/ContainerHeroSection/index.jsx
+++ src/components/ContainerHeroSection/index.jsx
@@ -1,21 +1,31 @@
-import React from 'react'
+import React from "react";
 
 //styles
-import './containerherosection.css'
+import "./containerherosection.css";
 
 function ContainerHeroSection() {
   return (
-    <div className='hero-main-container'>
-        <div className='focus-sec'>
-            <span>Focus</span>
-            <div className='hero-card'></div>
+    <div className="hero-main-container">
+      <div className="focus-sec">
+        <span>Focus</span>
+        <div className="focus-sec-card">
+          <img
+            src="../../../public/assets/img/image1.jpg"
+            alt="image1"
+            className="card-image"
+          ></img>
+          <span className="card-topic">Peaceful Piano</span>
+          <p className="card-para">
+            relax and indulge with beautiful piano pieces
+          </p>
         </div>
+      </div>
 
-        <div className='spotify-playlist-sec'>
-            <span>Spotify Playlists</span>
-        </div>
+      <div className="spotify-playlist-sec">
+        <span className="spotify-sec-card">Spotify Playlists</span>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ContainerHeroSection
+export default ContainerHeroSection;
