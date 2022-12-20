8155575622b923df5e2f0c1f52e7bab82b5951ee
svandu <svandu08@gmail.com>
2022-12-20 23:29:50 +0530

    hero section cards created

--- src/components/ContainerHeroSection/index.jsx
+++ src/components/ContainerHeroSection/index.jsx
@@ -14,6 +14,19 @@ function ContainerHeroSection() {
             alt="image1"
             className="card-image"
           ></img>
+          <svg
+            width="46"
+            height="46"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M3.806 6.206a4.8 4.8 0 0 1 6.788 0L12 7.612l1.406-1.406a4.8 4.8 0 1 1 6.788 6.788L12 21.188l-8.194-8.194a4.8 4.8 0 0 1 0-6.788Z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
           <span className="card-topic">Peaceful Piano</span>
           <p className="card-para">
             relax and indulge with beautiful piano pieces
@@ -21,9 +34,19 @@ function ContainerHeroSection() {
         </div>
       </div>
 
+      <span className="spotify-playlist">Spotify Playlist</span>
       <div className="spotify-playlist-sec">
-        <span className="spotify-sec-card">Spotify Playlists</span>
+        <div className="spotify-playlist-card">
+          <img
+            src="../../../public/assets/img/image1.jpg"
+            alt="image2"
+            className="spotify-card-image"
+          ></img>
+          <span className="spotify-card-topic">Today's top hits</span>
+          <p className="spotify-card-para">SZA is on top of the Hottest 50!</p>
+        </div>
       </div>
+      <hr></hr>
     </div>
   );
 }
