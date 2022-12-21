f38018500cd5bbaa1b240795359e865ada5cb3c0
svandu <svandu08@gmail.com>
2022-12-21 20:26:41 +0530

    hover effect added on the card

--- src/components/ContainerHeroSection/index.jsx
+++ src/components/ContainerHeroSection/index.jsx
@@ -9,11 +9,7 @@ function ContainerHeroSection() {
       <div className="focus-sec">
         <span>Focus</span>
         <div className="focus-sec-card">
-          <img
-            src="../../../public/assets/img/image1.jpg"
-            alt="image1"
-            className="card-image"
-          ></img>
+        <div className="focus-overlay-div">
           <svg
             width="46"
             height="46"
@@ -27,6 +23,12 @@ function ContainerHeroSection() {
               clip-rule="evenodd"
             ></path>
           </svg>
+        </div>
+          <img
+            src="../../../public/assets/img/image1.jpg"
+            alt="image1"
+            className="card-image"
+          ></img>
           <span className="card-topic">Peaceful Piano</span>
           <p className="card-para">
             relax and indulge with beautiful piano pieces
@@ -34,9 +36,24 @@ function ContainerHeroSection() {
         </div>
       </div>
 
-      <span className="spotify-playlist">Spotify Playlist</span>
       <div className="spotify-playlist-sec">
+      <span className="spotify-playlist">Spotify Playlist</span>
         <div className="spotify-playlist-card">
+        <div className="spotify-playlist-overlay-div">
+          <svg
+            width="46"
+            height="46"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M3.806 6.206a4.8 4.8 0 0 1 6.788 0L12 7.612l1.406-1.406a4.8 4.8 0 1 1 6.788 6.788L12 21.188l-8.194-8.194a4.8 4.8 0 0 1 0-6.788Z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </div>
           <img
             src="../../../public/assets/img/image1.jpg"
             alt="image2"
