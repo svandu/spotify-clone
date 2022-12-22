c872a50e3876ab13c0c2b7ec656eb8915d798cb1
svandu <svandu08@gmail.com>
2022-12-22 23:05:19 +0530

    modification on hero section

--- src/components/ContainerHeroSection/index.jsx
+++ src/components/ContainerHeroSection/index.jsx
@@ -9,21 +9,17 @@ function ContainerHeroSection() {
       <div className="focus-sec">
         <span>Focus</span>
         <div className="focus-sec-card">
-        <div className="focus-overlay-div">
-          <svg
-            width="46"
-            height="46"
-            fill="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M3.806 6.206a4.8 4.8 0 0 1 6.788 0L12 7.612l1.406-1.406a4.8 4.8 0 1 1 6.788 6.788L12 21.188l-8.194-8.194a4.8 4.8 0 0 1 0-6.788Z"
-              clip-rule="evenodd"
-            ></path>
-          </svg>
-        </div>
+          <div className="focus-overlay-div">
+            <svg
+              width="46"
+              height="46"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M12 2.25c-5.376 0-9.75 4.374-9.75 9.75s4.374 9.75 9.75 9.75 9.75-4.374 9.75-9.75S17.376 2.25 12 2.25Zm3.505 10.186-5.365 3.24a.506.506 0 0 1-.765-.435V8.76a.505.505 0 0 1 .765-.437l5.365 3.241a.51.51 0 0 1 0 .872Z"></path>
+            </svg>
+          </div>
           <img
             src="../../../public/assets/img/image2.png"
             alt="image1"
@@ -37,23 +33,19 @@ function ContainerHeroSection() {
       </div>
 
       <div className="spotify-playlist-sec">
-      <span className="spotify-playlist">Spotify Playlist</span>
+        <span className="spotify-playlist">Spotify Playlist</span>
         <div className="spotify-playlist-card">
-        <div className="spotify-playlist-overlay-div">
-          <svg
-            width="46"
-            height="46"
-            fill="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M3.806 6.206a4.8 4.8 0 0 1 6.788 0L12 7.612l1.406-1.406a4.8 4.8 0 1 1 6.788 6.788L12 21.188l-8.194-8.194a4.8 4.8 0 0 1 0-6.788Z"
-              clip-rule="evenodd"
-            ></path>
-          </svg>
-        </div>
+          <div className="spotify-playlist-overlay-div">
+            <svg
+              width="46"
+              height="46"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M12 2.25c-5.376 0-9.75 4.374-9.75 9.75s4.374 9.75 9.75 9.75 9.75-4.374 9.75-9.75S17.376 2.25 12 2.25Zm3.505 10.186-5.365 3.24a.506.506 0 0 1-.765-.435V8.76a.505.505 0 0 1 .765-.437l5.365 3.241a.51.51 0 0 1 0 .872Z"></path>
+            </svg>
+          </div>
           <img
             src="../../../public/assets/img/image3.png"
             alt="image2"
