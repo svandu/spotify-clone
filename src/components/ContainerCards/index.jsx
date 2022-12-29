24160d7b80ac8fe99cf1385cffdb9d5794f36c56
svandu <svandu08@gmail.com>
2022-12-29 23:34:35 +0530

    container cards component created

--- src/components/ContainerCards/index.jsx
+++ src/components/ContainerCards/index.jsx
@@ -1,11 +1,38 @@
 import React from 'react'
+import ContainerHeroSection from "./ContainerHeroSection";
 
 function ContainerCards() {
+    const song_details = [
+        {
+            name: "Peaceful Piano",
+            para: "relax and indulge with beautiful piano pieces",
+            img: "..public/assets/img/1stimage1.png"
+        },
+        {
+            name: "Deep Focus",
+            para: "Keep calm and focus with ambient and post-rock music.",
+            img: "..public/assets/img/1stimage2.png"
+        },
+        {
+            name: "Instrumental Study",
+            para: "Focus with soft study music in the background.",
+            img: "..public/assets/img/1stimage3.png"
+        },
+        {
+            name: "chilli lofi study beats",
+            para: "The perfect study beats. Find your focus, crush your productivity.",
+            img: "..public/assets/img/1stimage4.png"
+        },
+        {
+            name: "Coding Mode",
+            para: "Dedicated to all the programmers out there.",
+            img: "..public/assets/img/1stimage5.png"
+        },
+    ];
+
   return (
     <div>
-        {
-            
-        }
+        <ContainerHeroSection song_details = {songs}/>
     </div>
   )
 }
