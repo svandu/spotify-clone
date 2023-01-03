652db511af9099778c752106d19c48200ec3f9a4
svandu <svandu08@gmail.com>
2023-01-03 23:05:14 +0530

    button for show all added

--- src/components/ContainerCards/index.jsx
+++ src/components/ContainerCards/index.jsx
@@ -7,12 +7,11 @@ function ContainerCards(props) {
   return (
     <div className="card-conatiner">
       <span className="songs-field">{props.song.field}</span>
-      <button className="focus-button">show all</button>
+      {/* <button className="focus-button">show all</button> */}
       <div className="item-songs">
         {props.song.songlist.map((item, i) => (
           <div className="songs-cards" key={i}>
-            <div class
-            Name="cards-overlay">
+            <div className="cards-overlay">
               <svg
                 width="46"
                 height="46"
