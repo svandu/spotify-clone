bb056dae37a3c355b28142634775b1925a4668ea
svandu <svandu08@gmail.com>
2023-01-24 22:39:07 +0530

    image path add

--- src/components/RootPage/index.jsx
+++ src/components/RootPage/index.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import Demo from '../demo'
+// import logo from '../../assets/img/'
+
 import './rootpage.css'
 
 function RootPage() {
@@ -6,12 +9,13 @@ function RootPage() {
     <div className='root-main-container'>
         <div className='auth-container'>
             <div className='logo-sec'>
-                <img src='./public/assets/img/spotify.png' className='spotify-img'></img>
+                <img src='/public/assets/img/spotify.png' className='spotify-img'></img>
                 <p className='logo-name'>Spotify</p>
+                <Demo />
             </div>
             <div className='auth-btn'>                      
-                <button className='signup-btn'>signup</button>
-                <button className='login-btn'>login</button>
+                <button className='signup-btn'>sign up</button>
+                <button className='login-btn'>log in</button>
             </div>
         </div>
     </div>
