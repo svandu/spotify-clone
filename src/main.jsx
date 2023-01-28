3d17035d4195f24676f36674c50fd7e29860c785
svandu <svandu08@gmail.com>
2023-01-28 23:45:28 +0530

    react router dom add

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import HomePage from './pages/homepage'
+import { BrowserRouter } from "react-router-dom"
+// import HomePage from './pages/homepage'
 import './assets/styles/main.css'
 import RootPage from './pages/RootPage'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RootPage />
-    {/* <HomePage /> */}
-  </React.StrictMode>,
+    <BrowserRouter>
+      <RootPage />
+      {/* <HomePage /> */}
+    </BrowserRouter>
+  </React.StrictMode>
 )
