07a5a708cdd3fd86bf0058a9abe9102a502017d9
svandu <svandu08@gmail.com>
2023-01-29 23:12:35 +0530

    minor changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom"
-// import HomePage from './pages/homepage'
+import HomePage from './pages/homepage'
 import './assets/styles/main.css'
-import RootPage from './pages/RootPage'
+// import RootPage from './pages/RootPage'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <RootPage />
-      {/* <HomePage /> */}
+      {/* <RootPage /> */}
+      <HomePage />
     </BrowserRouter>
   </React.StrictMode>
 )
