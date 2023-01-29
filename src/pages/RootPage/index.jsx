0167acb88b09395bc21f3d1a7f8cd2c4050b25a8
svandu <svandu08@gmail.com>
2023-01-29 23:14:27 +0530

    path add

--- src/pages/RootPage/index.jsx
+++ src/pages/RootPage/index.jsx
@@ -1,30 +1,28 @@
-import React from 'react'
+import React from "react";
 
-//routing 
-import { Routes, Route } from "react-router-dom"
+//routing
+import { Link } from "react-router-dom";
 
-import styles from './rootpage.module.css';
+import styles from "./rootpage.module.css";
 
-import logo from '../../../public/assets/img/spotify.png';
-
-//authentication page 
-import LogIn from "./AuthPage/Login/index.jsx"
-import SignUp from "./AuthPage/SignUp/index.jsx"
+import logo from "../../../public/assets/img/spotify.png";
 
 function RootPage() {
   return (
     <div className={styles.root_container}>
       <div className={styles.logo_container}>
-          <img src={logo} className={styles.logo__img} />
-          <span className={styles.logo__title}>Spotify</span>
+        <img src={logo} className={styles.logo__img} />
+        <span className={styles.logo__title}>Spotify</span>
       </div>
-      <div className={styles.btn_container}>                      
-          <button className='btn btn--signup'>SIGN UP</button>
-          <button className='btn btn--login'>LOG IN</button>
+      <div className={styles.btn_container}>
+        <Link to="./AuthPage/SignUp">
+          <button className="btn btn--signup">SIGN UP</button>
+        </Link>
+
+        <Link to="./AuthPage/LogIn">
+          <button className="btn btn--login">LOG IN</button>
+        </Link>
       </div>
-      <Routes>
-      <Route path="./AuthPage/Login/index" element={<LogIn />}></Route>
-     </Routes>
     </div>
   );
 }
