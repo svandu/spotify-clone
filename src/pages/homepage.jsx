a95cd98409125ed5447aa0b961156f3e19b326df
svandu <svandu08@gmail.com>
2023-01-29 23:13:11 +0530

    router setup

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,25 +1,39 @@
-import React from "react"
-
+// import React from "react";
 //styles
 import "../assets/page-style/homepage.css";
 
-//components
-import SideNav from "../components/SideNav";
-import AuthFooter from "../components/AuthFooter";
-import MainContainer from "../components/MainContainer";
+//routing
+import { Routes, Route } from "react-router-dom";
 
-function Homepage() {
+//components
+// import SideNav from "../components/SideNav";
+// import AuthFooter from "../components/AuthFooter";
+// import MainContainer from "../components/MainContainer";
+import LogIn from "./AuthPage/LogIn";
+import SignUp from "./AuthPage/SignUp";
+import RootPage from "./RootPage";
+
+function HomePage() {
   return (
-    <div className="root-container">
-      <div className="home-main-container">
-        <SideNav />
-        <MainContainer />
-      </div>
-      <div className="footer">
-        <AuthFooter />
-      </div>
-    </div>
+    <>
+      <Routes>
+        <Route path="/RootPage" element={<RootPage />} />
+        <Route path="./AuthPage/LogIn" element={<LogIn />} />
+        <Route path="./AuthPage/SignUp" element={<SignUp />} />
+      </Routes>
+    </>
+
+
+    // <div className="root-container">
+    //   <div className="home-main-container">
+    //     <SideNav />
+    //     <MainContainer />
+    //   </div>
+    //   <div className="footer">
+    //     <AuthFooter />
+    //   </div>
+    // </div>
   );
 }
 
-export default Homepage;
+export default HomePage;
