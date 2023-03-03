275f232f49933dafba4d9e4c4e0244507ddaf1af
svandu <svandu08@gmail.com>
2023-03-03 12:35:09 +0530

    minor changes

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,38 +1,38 @@
-// import React from "react";
+import React from "react";
 //styles
-import "../assets/page-style/homepage.css";
+import "../assets/PageStyle/homepage.css";
 
 //routing
-import { Routes, Route } from "react-router-dom";
+// import { Routes, Route } from "react-router-dom";
 
 //components
-// import SideNav from "../components/SideNav";
-// import AuthFooter from "../components/AuthFooter";
-// import MainContainer from "../components/MainContainer";
-import LogIn from "./AuthPage/LogIn";
-import SignUp from "./AuthPage/SignUp";
-import RootPage from "./RootPage";
+import SideNav from "../components/SideNav";
+import AuthFooter from "../components/AuthFooter";
+import MainContainer from "../components/MainContainer";
+// import LogIn from "./AuthPage/LogIn";
+// import SignUp from "./AuthPage/SignUp";
+// import RootPage from "./RootPage";
 
 function HomePage() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<RootPage />} />
-        <Route path="/AuthPage/LogIn" element={<LogIn />} />
-        <Route path="/AuthPage/SignUp" element={<SignUp />} />
-      </Routes>
-    </>
+    // <>
+    //   <Routes>
+    //     <Route path="/" element={<RootPage />} />
+    //     <Route path="/AuthPage/LogIn" element={<LogIn />} />
+    //     <Route path="/AuthPage/SignUp" element={<SignUp />} />
+    //   </Routes>
+    // </>
 
 
-    // <div className="root-container">
-    //   <div className="home-main-container">
-    //     <SideNav />
-    //     <MainContainer />
-    //   </div>
-    //   <div className="footer">
-    //     <AuthFooter />
-    //   </div>
-    // </div>
+    <div className="root-container">
+      <div className="home-main-container">
+        <SideNav />
+        <MainContainer />
+      </div>
+      <div className="footer">
+        <AuthFooter />
+      </div>
+    </div>
   );
 }
 
